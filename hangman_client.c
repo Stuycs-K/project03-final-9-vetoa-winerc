cf50c8cafcc50941039e1cd66082757999472630
aveto40 <aveto40@stuy.edu>
2024-01-04 23:14:20 -0500

    added networking and handshakes

--- hangman_client.c
+++ hangman_client.c
@@ -1,1 +1,10 @@
+#include "networking.h"
 
+int main(int argc, char** argv) {
+    char* IP = "127.0.0.1";
+    if(argc>1){
+        IP=argv[1];
+    }
+    int server_socket = client_tcp_handshake(IP);
+    printf("client connected.\n");
+}
--- hangman_server.c
+++ hangman_server.c
@@ -1,54 +1,4 @@
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <time.h>
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/select.h>
-#define MAX_CLIENTS 8
-#define WORD_SIZE 128
-#define MESSAGE_SIZE 512
-
-void error(int number, char* message) {
-    if (number == -1) {
-        printf("%s\n", message);
-        printf("%s\n", strerror(errno));
-        exit(1);
-    }
-}
-
-/*
-    creates the server socket for clients to connect to
-*/
-int server_setup() {
-    struct addrinfo * hints, * results;
-    hints = calloc(1,sizeof(struct addrinfo));
-    char* PORT = "1738";
-
-    hints->ai_family = AF_INET;
-    hints->ai_socktype = SOCK_STREAM; //TCP socket
-    hints->ai_flags = AI_PASSIVE; //only needed on server
-    error(getaddrinfo(NULL, PORT , hints, &results), "getaddrinfo failed");  //NULL is localhost or 127.0.0.1
-
-    //create socket
-    int listen_socket = socket(results->ai_family, results->ai_socktype, results->ai_protocol);\
-
-    //this code allows the port to be freed after program exit.  (otherwise wait a few minutes)
-    int yes = 1;
-    error(( setsockopt(listen_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1 ), "setsockopt failed");
-
-    error(bind(listen_socket, results->ai_addr, results->ai_addrlen), "bind failed");
-    listen(listen_socket, 3);//3 clients can wait to be processed
-    printf("Listening on port %s\n",PORT);
-
-    free(hints);
-    freeaddrinfo(results);
-    return listen_socket;
-}
+#include "networking.h"
 
 /*
     Arguments: the file descriptor of the socket connection that the command is coming from
@@ -62,11 +12,6 @@ void handle_command(int client_socket) {
 int main(){
     // opens the socket for clients to connect to
     int listen_socket = server_setup();
-
-    socklen_t sock_size;
-    struct sockaddr_storage client_address;
-    sock_size = sizeof(client_address);
-
     fd_set read_fds;
 
     // initializes arrays of usernames and of sockets to clients
@@ -101,7 +46,7 @@ int main(){
             }
             else {
                 //accept the connection
-                int client_socket = accept(listen_socket,(struct sockaddr *)&client_address, &sock_size);
+                int client_socket = server_tcp_handshake(listen_socket);
                 printf("Connected to new client\n");
 
                 // add the new socket to the list of socket connections
