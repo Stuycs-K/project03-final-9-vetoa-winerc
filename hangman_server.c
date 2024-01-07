c72f5a591889f5caf1a2cd26e9c909748cfef180
aveto40 <aveto40@stuy.edu>
2024-01-07 00:13:51 -0500

    added server commands for changing gamemode and chooser

--- hangman_server.c
+++ hangman_server.c
@@ -12,11 +12,52 @@ void client_command(int client_socket, struct game_info* game) {
 }
 
 /*
-    Arguments: none
-    Behavior: handles commands from the server given through stdin
-    Returns: none
+    Arguments: the struct containing the game info of the current game
+    Behavior: prompts the user for input, then changes the gamemode accordingly
+    Returns: the struct containing the game info after changing gamemode
 */
-void server_command(struct game_info* game) {
+struct game_info* change_gamemode(struct game_info* game) {
+    // printf("old gamemode: %d\n", game->gamemode);
+    printf("New gamemode ('computer' or 'user'): ");
+    char buff[20];
+    fgets(buff, 19, stdin);
+    buff[strcspn(buff, "\n")] = 0;
+    if (strcmp(buff, "computer") == 0) {
+        game->gamemode = COMPUTER_CHOOSING;
+    }
+    else if (strcmp(buff, "user") == 0) {
+        game->gamemode = USER_CHOOSING;
+    }
+    else {
+        printf("invalid gamemode, changing gamemode failed\n");
+    }
+    // printf("new gamemode: %d\n", game->gamemode);
+    return game;
+}
+
+struct game_info* change_chooser(struct game_info* game) {
+    printf("old chooser: %d\n", game->chooser);
+    printf("Enter the index of the new chooser: ");
+    char buff[3];
+    fgets(buff, 3, stdin);
+    buff[strcspn(buff, "\n")] = 0;
+    int newChooser = atoi(buff);
+    if (newChooser < game->num_clients && newChooser >= 0) {
+        game->chooser = newChooser;
+    }
+    else {
+        printf("new chooser doesn't exist, command failed\n");
+    }
+    printf("new chooser: %d\n", game->chooser);
+    return game;
+}
+
+/*
+    Arguments: the struct with the current game info
+    Behavior: handles commands from the server given through stdin
+    Returns: the struct with the new game info
+*/
+struct game_info* server_command(struct game_info* game) {
     char command[COMMAND_SIZE] = "";
     read(fileno(stdin), command, COMMAND_SIZE);
     command[strcspn(command, "\n")] = 0;
@@ -32,12 +73,16 @@ void server_command(struct game_info* game) {
     else if (strcmp(command, "start") == 0) {
         startGame(game);
     }
-    else if (strcmp(command, "")) {
-
+    else if (strcmp(command, "gamemode") == 0) {
+        game = change_gamemode(game);
+    }
+    else if (strcmp(command, "chooser") == 0) {
+        game = change_chooser(game);
     }
     else {
         printf("Invalid command. For instructions, type 'help'\n");
     }
+    return game;
 }
 
 int main(){
@@ -46,10 +91,12 @@ int main(){
     fd_set read_fds;
     struct game_info* game = malloc(sizeof(struct game_info*));
     // computer word chooser
-    game->gamemode = 0;
+    game->gamemode = COMPUTER_CHOOSING;
     game->num_clients = 0;
     game->client_sockets = malloc(sizeof(int) * MAX_CLIENTS);
     game->usernames = malloc(sizeof(char*) * MAX_CLIENTS);
+    // first user to join is automatically the chooser
+    game->chooser = 0;
     for (int i = 0; i < MAX_CLIENTS; i++) {
         game->usernames[i] = malloc(WORD_SIZE);
     }
--- hangman_server.h
+++ hangman_server.h
@@ -5,12 +5,17 @@ struct game_info {
     int* client_sockets;
     char** usernames;
     char gamemode;
+    int chooser;
 };
 #endif
 
 #ifndef HANGMAN_SERVER_H
 #define HANGMAN_SERVER_H
 void client_command(int client_socket, struct game_info* game);
-void server_command(struct game_info* game);
-
+struct game_info* server_command(struct game_info* game);
+struct game_info* change_gamemode(struct game_info* game);
+struct game_info* change_chooser(struct game_info* game);
 #endif
+
+#define COMPUTER_CHOOSING 0
+#define USER_CHOOSING 1
