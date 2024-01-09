4c5c76d99659fe3f897e8830c9c438525a035dcd
aveto40 <aveto40@stuy.edu>
2024-01-09 12:09:53 -0500

    adrian hangman.c shell for compiling

---
+++ hangman.c
@@ -0,0 +1,16 @@
+#include "hangman_server.h"
+char checkLetterGuess(char letter) {
+ return '*';
+}
+
+char checkWordGuess(char* target) {
+ return '*';
+}
+
+void guessResult(int result) {
+ return;
+}
+
+void startGame(struct game_info* game) {
+    return;
+}
