0dfa1928ec1bfcb5baad39ef33d7ac40e78edf24
charliewiner <cwiner40@stuy.edu>
2024-01-09 14:10:19 -0500

    need to test

--- hangman.c
+++ hangman.c
@@ -8,62 +8,32 @@ void err(int line) {
   exit(1);
 }
 
-char checkLetterGuess(char letter) {
-    FILE *file;
-    char word[21];//max word length of 20 characters
-    char guessProgress[21]; //guess progress (asterisks represent unknowns)
-    int result = 0;
-    int guesses = 0;
-
-    file = fopen("word.txt", "r+"); //use r+ to read and write
-    if(file == NULL) err(16);
-    
-    if(fscanf(file, "%20s", word) != 1) err(20);//incorrectly formatted word txt file
-
-    if(fscanf(file, "%20s", guessProgress) != 1) { //fill in guess progress w/ asterisks (handling for first guess)
-      for(int i = 0; word[i] != '\0'; i++) {
-          guessProgress[i] = '*';
-      }
-      guessProgress[strlen(word)] = '\0';
+struct game_info* checkLetterGuess(struct game_info* game, char letter) {
+  char success = 0; // not a success
+  for (int i = 0; i < strlen(game->real_word); i++) {
+    if (game->real_word[i] == letter) {
+      game->current_word[i] = letter;
+      success = 1;
     }
+  }
 
-    if(fscanf(file, "%d", &guesses) != 1) {
-      guesses = 0;
-    }
-    guesses++;
-
-    for(int i = 0; word[i] != '\0'; i++) {
-      if(word[i] == letter) {
-        guessProgress[i] = letter;
-        result++; //letter found
-      }
-    }
-
-    rewind(file); // go back to the start of the file so you can overwrite guessprogress.
-    fprintf(file,"%s\n%s\n%d\n",word, guessProgress, guesses);
-    fclose(file);
-    // guessResult(result);
-    return result;
+  if (success == 0) {
+    game->num_guesses--;
+  }
+  return game;
 }
 
-char checkWordGuess(char* target) {
-  FILE *r_file;
-  char word[21];//max word length of 20 characters
-  int result = 0;
-  r_file = fopen("word.txt", "r");
-  if(r_file == NULL) {
-      err(40);
+struct game_info* checkWordGuess(struct game_info* game, char* target) {
+  char success = 0;
+  if (strcmp(game->real_word, target) == 0) {
+    game->current_word = target;
+    success = 1;
+  }
+  if (success == 0) {
+    game->num_guesses--;
   }
 
-  if(fscanf(r_file, "%20s", word) != 1) err(36);//incorrectly formatted word txt file
-  else {
-    if(!strcasecmp(word, target))
-      result++; //words don't match so result is false
-  }
-  guessResult(result);
-  return result; 
-
-  
+  return game;
 }
 
 void guessResult(int result) {
